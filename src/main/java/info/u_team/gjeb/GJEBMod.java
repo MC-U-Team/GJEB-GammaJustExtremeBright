ea1d58e96a57286ee480babf219395218836a043
HyCraftHD <hycrafthd@live.de>
2020-06-27 01:20:46 +0200

    Use the safe method

--- src/main/java/info/u_team/gjeb/GJEBMod.java
+++ src/main/java/info/u_team/gjeb/GJEBMod.java
@@ -20,7 +20,7 @@ public class GJEBMod {
 	
 	public GJEBMod() {
 		tryCheckSigned();
-		DistExecutor.runWhenOn(Dist.CLIENT, () -> GJEBMod::replaceSlider);
+		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> GJEBMod::replaceSlider);
 	}
 	
 	private void tryCheckSigned() {
