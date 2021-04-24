b62904f26e887eb83ce10b7b24bb3acd85e87170
HyCraftHD <hycrafthd@live.de>
2021-04-25 01:22:03 +0200

    Use no * in imports anymore

--- src/main/java/info/u_team/gjeb/GJEBMod.java
+++ src/main/java/info/u_team/gjeb/GJEBMod.java
@@ -3,11 +3,15 @@ package info.u_team.gjeb;
 import java.lang.reflect.Method;
 
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import info.u_team.gjeb.client.GJEBReplace;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.*;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 
