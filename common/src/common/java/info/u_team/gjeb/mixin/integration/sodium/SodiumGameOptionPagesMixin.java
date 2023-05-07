c10fea5ed2a8d46712a2efe1f4e1ee90b7978ace
HyCraftHD <hycrafthd@live.de>
2023-05-07 02:20:49 +0200

    Add @Pseudo for optional

--- common/src/common/java/info/u_team/gjeb/mixin/integration/sodium/SodiumGameOptionPagesMixin.java
+++ common/src/common/java/info/u_team/gjeb/mixin/integration/sodium/SodiumGameOptionPagesMixin.java
@@ -1,9 +1,11 @@
 package info.u_team.gjeb.mixin.integration.sodium;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 
 import info.u_team.gjeb.mixin.GJEBAbstractMixinPlugin;
 
+@Pseudo
 @Mixin(targets = GJEBAbstractMixinPlugin.SODIUM_CLASS)
 abstract class SodiumGameOptionPagesMixin {
 	
